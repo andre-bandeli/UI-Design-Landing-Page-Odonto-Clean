6fc548d6705af014e1d9e69e392c79b578623cf5
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-05 18:46:38 -0300

    seccionando nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './componentes/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     
     </div>
   );
---
+++ src/componentes/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbar.scss'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+
+        <div className="container">
+            <div className="col-logo">
+
+            </div>
+            <div className="nav">
+
+            </div>
+            <div className="btn-agendar">
+                
+            </div>
+        </div>
+
+    </div>
+  )
+}
