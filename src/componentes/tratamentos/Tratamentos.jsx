1abe0dac6711bcc6f7e127bbfb929ced7d287c47
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-06 01:00:00 -0300

    feat: tratamentos

--- src/componentes/tratamentos/Tratamentos.jsx
+++ src/componentes/tratamentos/Tratamentos.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
 import './tratamentos.scss'
+import { FaCheck } from 'react-icons/fa';
 
 export default function Tratamentos() {
   return (
     <div className='tratamentos'>
+      <h2>Tratamentos</h2>
         <div className="container">
             <div className="box">
 
+              <div className="titulo">
+                  <div className="fa-icon">
+                    <FaCheck/>
+                  </div>
+                  <h3>Implantandontia</h3>
+              </div>
+              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                 Saepe, nostrum sint! Eius repellat molestiae quisquam optio, deleniti reiciendis 
+                distinctio beatae accusamus corrupti harum, sed placeat ea repellendus. Aperiam, quod iste.</h3>
+
             </div>
 
             <div className="box">
+            <div className="titulo">
+                  <div className="fa-icon">
+                    <FaCheck/>
+                  </div>
+                  <h3>Implantandontia</h3>
+              </div>
+              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                 Saepe, nostrum sint! Eius repellat molestiae quisquam optio, deleniti reiciendis 
+                distinctio beatae accusamus corrupti harum, sed placeat ea repellendus. Aperiam, quod iste.</h3>
                 
             </div>
             <div className="box">
-                
+            <div className="titulo">
+                  <div className="fa-icon">
+                    <FaCheck/>
+                  </div>
+                  <h3>Implantandontia</h3>
+              </div>
+              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                 Saepe, nostrum sint! Eius repellat molestiae quisquam optio, deleniti reiciendis 
+                distinctio beatae accusamus corrupti harum, sed placeat ea repellendus. Aperiam, quod iste.</h3>
             </div>
             <div className="box">
-                
+            <div className="titulo">
+                  <div className="fa-icon">
+                    <FaCheck/>
+                  </div>
+                  <h3>Implantandontia</h3>
+              </div>
+              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                 Saepe, nostrum sint! Eius repellat molestiae quisquam optio, deleniti reiciendis 
+                distinctio beatae accusamus corrupti harum, sed placeat ea repellendus. Aperiam, quod iste.</h3>
             </div>
             
         </div>
