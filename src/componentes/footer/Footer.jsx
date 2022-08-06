6b89a7f4acd0fdf1178ba4adf095c04fbfe4ef5c
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-06 03:46:22 -0300

    feat: footer

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -6,6 +6,46 @@ export default function Footer() {
   return (
     <div className='footer'>
 
+      <div className="container">
+          <div className="col-sm">
+
+            <img src="imagens/logo.png" alt="" />
+
+          </div>
+          <div className="col-sm">
+
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ut atque sed numquam aspernatur accusantium</h3>
+
+          </div>
+          <div className="col-sm">
+
+            <h2>Páginas</h2>
+            <ul>
+              <li>
+                  Home
+              </li>
+              <li>
+                  Sobre
+              </li>
+              <li>
+                  Profissionais
+              </li>
+              <li>
+                  Tratamentos
+              </li>
+              <li>
+                  Dúvidas
+              </li>
+            </ul>
+
+          </div>
+          <div className="col-sm right">
+                  <img src="icones/facebook.png" alt="" />
+                  <img src="icones/linkedin.png" alt="" />
+                  <img src="icones/instagram.png" alt="" />
+          </div>
+      </div>
+
     </div>
   )
 }
