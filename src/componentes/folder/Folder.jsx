dac403927ecb6aede1752f39abee342d15aa42f2
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-05 22:25:44 -0300

    merge develop para main

---
+++ src/componentes/folder/Folder.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './folder.scss'
+
+export default function Folder() {
+  return (
+    <div className='folder'>
+        <div className="container"></div>
+    </div>
+  )
+}
