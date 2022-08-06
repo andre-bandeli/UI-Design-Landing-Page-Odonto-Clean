6aa74a31c04b2799990a8f959308234783b68b8b
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-06 00:49:05 -0300

    feat: cards

--- src/componentes/cards/Cards.jsx
+++ src/componentes/cards/Cards.jsx
@@ -5,25 +5,52 @@ export default function Cards() {
   return (
     <div className='cards'>
 
+      <h2>Diferencial</h2>
+
         <div className="container">
 
             <div className="box">
 
+              <div className="circle">
+                    <img src="icones/coracao.png" alt="" />
+              </div>
+              <h2>Profissionais Capacitados</h2>
+              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate assumenda consequuntur cum, et architecto nihil odit</h3>
             </div>
             <div className="box">
-
+              <div className="circle">
+                      <img src="icones/agenda.png" alt="" />
+                </div>
+                <h2>Profissionais Capacitados</h2>
+                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate assumenda consequuntur cum, et architecto nihil odit</h3>
             </div>
             <div className="box">
-
+                <div className="circle">
+                        <img src="icones/dente.png" alt="" />
+                  </div>
+                  <h2>Profissionais Capacitados</h2>
+                  <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate assumenda consequuntur cum, et architecto nihil odit</h3>
             </div>
             <div className="box">
-
+                  <div className="circle">
+                          <img src="icones/fone-de-ouvido.png" alt="" />
+                    </div>
+                    <h2>Profissionais Capacitados</h2>
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate assumenda consequuntur cum, et architecto nihil odit</h3>
             </div>
             <div className="box">
-
+                    <div className="circle">
+                            <img src="icones/equipe.png" alt="" />
+                      </div>
+                      <h2>Profissionais Capacitados</h2>
+                      <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate assumenda consequuntur cum, et architecto nihil odit</h3>
             </div>
             <div className="box">
-
+                      <div className="circle">
+                            <img src="icones/exame-medico.png" alt="" />
+                      </div>
+                      <h2>Profissionais Capacitados</h2>
+                      <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate assumenda consequuntur cum, et architecto nihil odit</h3>
             </div>
         </div>
     </div>
