e6cd2d601b4f51a7e65a63ba4c1715d423121412
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-05 21:57:27 -0300

    seccionando slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './componentes/navbar/Navbar';
+import Slider from './componentes/slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Slider/>
     
     </div>
   );
---
+++ src/componentes/slider/Slider.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './slider.scss'
+
+export default function Slider() {
+  return (
+    <div className='slider'>
+
+        <div className="container">
+            <div className="col-6">
+
+            </div>
+            <div className="col-6">
+                
+            </div>
+        </div>
+
+    </div>
+  )
+}
