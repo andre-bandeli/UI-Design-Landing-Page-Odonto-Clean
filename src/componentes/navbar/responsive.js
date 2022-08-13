632951f06beb6c0a34478eab0651d8c8659b22b4
andre-bandeli <andre.lbandeli@gmail.com>
2022-08-13 02:28:27 -0300

    gh_pages

--- src/componentes/navbar/responsive.js
+++
@@ -1,6 +0,0 @@
-// jquery ao clicar abre e fecha div (lembrar de display:none na div)
-$(document).ready(function(){
-    $("#bt-show-taxas").click(function(){
-      $("#show-taxas").toggle();
-    });
-  });
